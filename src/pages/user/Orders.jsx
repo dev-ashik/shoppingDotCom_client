bd1165727769bc0523b9eba6938bf1c7da07c20b
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-04-29 01:12:02 +0800

    show all order data

--- src/pages/user/Orders.jsx
+++ src/pages/user/Orders.jsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import MenuOfUsers from "../../components/Layout/MenuOfUsers";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
-  const [orderProducts, setOrderProducts] = useState([]);
 
   const getOrders = async () => {
-    const {data} = await axios.get("http://localhost:8000/api/v1/product/orders")
+    const { data } = await axios.get(
+      "http://localhost:8000/api/v1/product/orders"
+    );
 
-    if(data.success) {
+    if (data.success) {
       setOrders(data.orders);
     }
-  }
+  };
 
   useEffect(() => {
     getOrders();
-  }, [])
+  }, []);
 
   return (
     <Layout>
@@ -30,48 +32,45 @@ const Orders = () => {
             <h2>All orders</h2>
 
             <div className="">
-              {
-                orders.map(product => (
-                  <div key={product._id} className="m-2 p-2" style={{border: "1px solid black"}}>
-                    {
-                    product.products.length > 1 ? (
-                      product.products.map((pId) => <div className="">
+              {orders.map((order) => (
+                <div
+                  key={order._id}
+                  className="m-2 p-2"
+                  style={{ border: "1px solid black" }}
+                >
+                  {
+                  order.products.map((item) => (
+                    <Link to={`/product/${item.slug}`} style={{ border: "1px solid black" }}>
                       <img
-                    src={`http://localhost:8000/api/v1/product/product-photo/${pId}`}
-                    className="card-img-top m-2"
-                    alt="product image"
-                    style={{
-                      height: "200px",
-                      width: "200px",
-                      objectFit: "contain",
-                      backgroundImage:
-                        "linear-gradient(120deg, #d6e6ff 0%, #cfeffd 100%)",
-                    }}
-                  />
-                      </div>)
-                    ) : (<>
-                    <img
-                    src={`http://localhost:8000/api/v1/product/product-photo/${product.products}`}
-                    className="card-img-top m-2"
-                    alt="product image"
-                    style={{
-                      height: "200px",
-                      width: "200px",
-                      objectFit: "contain",
-                      backgroundImage:
-                        "linear-gradient(120deg, #d6e6ff 0%, #cfeffd 100%)",
-                    }}
-                  />
-                    </>)
-                    }
-                    <p>address: {product.address}</p>
-                    <p>payment: {product.payment}</p>
-                    <p>Time: { new Date(product.createdAt).getDate()}-{ new Date(product.createdAt).getMonth() + 1}-{ new Date(product.createdAt).getFullYear()} <small>dd/mm/yy</small></p>
+                        src={`http://localhost:8000/api/v1/product/product-photo/${item._id}`}
+                        className="card-img-top m-2"
+                        alt="product image"
+                        style={{
+                          height: "200px",
+                          width: "200px",
+                          objectFit: "contain",
+                          backgroundImage:
+                            "linear-gradient(120deg, #d6e6ff 0%, #cfeffd 100%)",
+                        }}
+                      />
+                      <p>Name: {item.name}</p>
+                      <p>price: {item.price}</p>
+                    </Link>
+                  ))
+                  }
 
-                    <p>status: {product.status}</p>
-                  </div>
-                ))
-              }
+                  <p>address: {order.address}</p>
+                  <p>payment: {order.payment}</p>
+                  <p>
+                    Time: {new Date(order.createdAt).getDate()}-
+                    {new Date(order.createdAt).getMonth() + 1}-
+                    {new Date(order.createdAt).getFullYear()}{" "}
+                    <small>dd/mm/yy</small>
+                  </p>
+
+                  <p>status: {order.status}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
